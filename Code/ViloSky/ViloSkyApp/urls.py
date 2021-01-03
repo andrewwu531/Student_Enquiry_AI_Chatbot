5cbabb39dd4d650098ea4912b81c2d41a208ed9b
Selim Hadjadj-Aoul <2396794h@student.gla.ac.uk>
2021-01-03 17:18:49 +0000

    Rough login and register page added

--- Code/ViloSky/ViloSkyApp/urls.py
+++ Code/ViloSky/ViloSkyApp/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/',views.login, name='login'),
+    path('register/',views.register, name='register'),
 ]
--- Code/ViloSky/ViloSkyApp/views.py
+++ Code/ViloSky/ViloSkyApp/views.py
@@ -7,3 +7,9 @@ from django.conf import settings
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {}) 
+
+def login(request):
+    return render(request, 'login.html', {}) 
+
+def register(request):
+    return render(request, 'register.html', {}) 
