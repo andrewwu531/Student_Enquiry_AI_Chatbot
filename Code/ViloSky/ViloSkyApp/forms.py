1e78620c2dcaf60134f10a2a9d204ce04b76a3ae
Selim Hadjadj-Aoul <2396794h@student.gla.ac.uk>
2021-02-04 12:49:46 +0000

    Registration form -working

--- Code/ViloSky/ViloSkyApp/forms.py
+++ Code/ViloSky/ViloSkyApp/forms.py
@@ -1,12 +1,11 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from ViloSkyApp.models import UserProfile
 class UserForm(forms.ModelForm):
-    username = forms.CharField(widget = forms.TextInput(attrs = {'class' : 'form-control txtbox'}))
     email = forms.CharField(widget = forms.EmailInput(attrs = {'class' : 'form-control txtbox'}))
     password = forms.CharField(widget = forms.PasswordInput(attrs = {'class' : 'form-control txtbox'}))
     confirm_password = forms.CharField(widget = forms.PasswordInput(attrs = {'class' : 'form-control txtbox'}))
 
     class Meta:
-        model = User
-        fields = ('username', 'email', 'password', 'confirm_password')
+        model = get_user_model()
+        fields = ( 'email', 'password', 'confirm_password')
