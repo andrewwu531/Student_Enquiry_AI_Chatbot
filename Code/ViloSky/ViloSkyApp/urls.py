85da1399627695686110fb7a3a7bdc7fd30f97d8
2390362s <2390362s@student.gla.ac.uk>
2021-02-16 19:03:15 +0000

    Generic list implementation and fixes
     * Create generic list component (template, js, styling)
     * Rename action_plan, admin_input, and reports templates needed.
     * Change base, baseuser to allow for custom css/js files for further
    components.
     * Scaffold a quick error template for err conditions.
     * Instantiate generic_list for admin inputs, reports, and action plans
     * Change URLs and Views accordingly.
    
    See #62

--- Code/ViloSky/ViloSkyApp/urls.py
+++ Code/ViloSky/ViloSkyApp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.urls import include
 from django.conf import settings
 from ViloSkyApp import views
@@ -6,17 +6,20 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/',views.login, name='login'),
-    path('register/',views.register, name='register'),
-    path('dashboard/',views.dashboard, name='dashboard'),
-    path('mydetails/',views.mydetails, name='mydetails'),
-    path('myactions/',views.myactions, name='myactions'),
-    path('report/',views.report, name='report'),
-    path('roles/',views.roles, name='roles'),
-    path('input/',views.input, name='input'),
-    path('output/',views.output, name='output'),
-    path('data/',views.data, name='data'),
-    path('editquestion/',views.editquestion, name='editquestion'),
-    path('outputdetails/',views.outputdetails, name='outputdetails'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('mydetails/', views.mydetails, name='mydetails'),
+    re_path('action/(?P<action_id>\w+)/$', views.action, name='action'),
+    path('actions/', views.actions, name='actions'),
+    re_path('report/(?P<report_id>\w+)/$', views.report, name='report'),
+    path('reports/', views.reports, name='reports'),
+    path('roles/', views.roles, name='roles'),
+    re_path('admin_input/(?P<admin_input_id>\w+)/$',
+            views.admin_input, name='admin_input'),
+    path('admin_inputs/', views.admin_inputs, name='admin_inputs'),
+    path('output/', views.output, name='output'),
+    path('data/', views.data, name='data'),
+    path('outputdetails/', views.outputdetails, name='outputdetails'),
     path('logout/', views.user_logout, name='logout'),
 ]
--- Code/ViloSky/ViloSkyApp/views.py
+++ Code/ViloSky/ViloSkyApp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
 from django.conf import settings
+from django.core import serializers
 from plotly.offline import plot
 import plotly.graph_objs as go
 from random import randint
@@ -12,11 +13,15 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as ulogin
 from django.contrib.auth import logout as ulogout
 from django.contrib.auth.decorators import login_required
-import ViloSkyApp.models
+from django.utils.formats import localize
+from ViloSkyApp import models
 from ViloSkyApp.forms import UserForm
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html', {}) 
+    return render(request, 'index.html', {})
+
 
 def login(request):
     context_dict = {}
@@ -24,7 +29,7 @@ def login(request):
         if request.method == 'POST':
             email = request.POST.get('email')
             password = request.POST.get('password')
-            user = authenticate(username = email, password = password)
+            user = authenticate(username=email, password=password)
             if user:
                 ulogin(request, user)
                 return redirect(reverse('dashboard'))
@@ -35,7 +40,8 @@ def login(request):
             return render(request, 'login.html', context_dict)
     else:
         return redirect(reverse('dashboard'))
-    return render(request, 'login.html', {}) 
+    return render(request, 'login.html', {})
+
 
 def register(request):
     if not request.user.is_authenticated:
@@ -48,64 +54,133 @@ def register(request):
                 user.save()
                 if request.POST.get('tos') != "on":
                     user.delete()
-                    messages.error(request, "You must accept the TOS and Privacy Policy in order to register.")
+                    messages.error(
+                        request, "You must accept the TOS and Privacy Policy in order to register.")
                 elif request.POST.get('password') != request.POST.get('confirm_password'):
                     user.delete()
-                    messages.error(request, "The passwords provided do not match.")
+                    messages.error(
+                        request, "The passwords provided do not match.")
                 else:
                     registered = True
         else:
             user_form = UserForm()
         context_dict = {
-            'user_form' : user_form,
-            'registered' : registered,
+            'user_form': user_form,
+            'registered': registered,
         }
         return render(request, 'register.html', context_dict)
     else:
         return redirect(reverse('dashboard'))
+
 
 @login_required(login_url='login')
 def user_logout(request):
     ulogout(request)
     return redirect(reverse('index'))
 
+
 @login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'dashboard.html', {}) 
+    return render(request, 'dashboard.html', {})
 
-@login_required(login_url='login')    
+
+@login_required(login_url='login')
 def mydetails(request):
-    return render(request, 'mydetails.html', {}) 
+    return render(request, 'mydetails.html', {})
+
 
 @login_required(login_url='login')
-def myactions(request):
-    return render(request, 'myactions.html', {}) 
+def action(request, action_id):
+    # Pass required info and update template
+    return render(request, 'action_plan.html', {'action_id': action_id})
+
 
 @login_required(login_url='login')
-def report(request):
-    return render(request, 'report.html', {}) 
+def actions(request):
+    user = models.UserProfile.objects.filter(user=request.user).first()
+
+    if user is None:
+        return render(request, "error.html")
+
+    user_reports = user.reports_assigned.all().order_by('-datetime_created')
+    entries = [{"id": report.id, "title": f"Action plan from {localize(report.datetime_created)}"}
+               for report in user_reports]
+    template_headings = ["#", "Title"]
+    model_keys = ["id", "title"]
+
+    return render(request, 'action_plans.html', {
+        "headings": template_headings, "model_keys": model_keys,
+        "entries": entries, "row_link_to": "/action/"
+    })
+
 
 @login_required(login_url='login')
+def report(request, report_id):
+    # Pass required info and update template
+    return render(request, 'report.html', {})
+
+
+@login_required(login_url='login')
+def reports(request):
+    user = models.UserProfile.objects.filter(user=request.user).first()
+
+    if user is None:
+        return render(request, "error.html")
+
+    reports_to_render = list(
+        user.reports_assigned.all().values().order_by('-datetime_created'))
+    template_headings = ["#", "Date Created"]
+    model_keys = ["id", "datetime_created"]
+
+    return render(request, 'reports.html', {
+        "headings": template_headings, "model_keys": model_keys,
+        "entries": reports_to_render, "row_link_to": "/report/"
+    })
+
+
+@ login_required(login_url='login')
 def roles(request):
-    return render(request, 'roles.html', {}) 
+    return render(request, 'roles.html', {})
 
-@login_required(login_url='login')
-def input(request):
-    return render(request, 'input.html', {}) 
 
-@login_required(login_url='login')
+@ login_required(login_url='login')
+def admin_input(request, admin_input_id):
+    # Pass required info and update template
+    return render(request, 'admin_input.html', {'admin_input_id': admin_input_id})
+
+
+@ login_required(login_url='login')
+def admin_inputs(request):
+    user = models.UserProfile.objects.filter(user=request.user).first()
+
+    if user is None:
+        return render(request, "error.html")
+
+    inputs_to_render = list(
+        models.AdminInput.objects.all().values('id', 'created_by__user__first_name', 'label', 'input_type', 'is_required'))
+    template_headings = ["#", "Created By", "Label", "Type", "Required"]
+    model_keys = ["id", "created_by__user__first_name",
+                  "label", "input_type", "is_required"]
+
+    return render(request, 'admin_inputs.html', {
+        "headings": template_headings, "model_keys": model_keys,
+        "entries": inputs_to_render, "row_link_to": "/admin_input/"
+    })
+
+    return render(request, 'admin_inputs.html', {})
+
+
+@ login_required(login_url='login')
 def output(request):
-    return render(request, 'output.html', {}) 
+    return render(request, 'output.html', {})
 
-@login_required(login_url='login')
-def editquestion(request):
-    return render(request, 'editquestion.html', {}) 
 
-@login_required(login_url='login')
+@ login_required(login_url='login')
 def outputdetails(request):
-    return render(request, 'outputdetails.html', {}) 
+    return render(request, 'outputdetails.html', {})
 
-@login_required(login_url='login')
+
+@ login_required(login_url='login')
 def data(request):
     visitors = []
     registered_users = []
@@ -114,18 +189,18 @@ def data(request):
     days = []
     for i in range(31):
         days.append(i)
-        cur_visitors = randint(0,12000)
+        cur_visitors = randint(0, 12000)
         visitors.append(cur_visitors)
-        registered_users.append(int(cur_visitors*uniform(0,0.4)))
-        inputs.append(randint(100,2900))
-        outputs.append(randint(200,8000))
-    vis_fig = go.Figure(data=[go.Scatter(x=days,y=visitors, name="visitors"),
-                        go.Scatter(x=days,y=registered_users, name="registered users")]
-    )
+        registered_users.append(int(cur_visitors*uniform(0, 0.4)))
+        inputs.append(randint(100, 2900))
+        outputs.append(randint(200, 8000))
+    vis_fig = go.Figure(data=[go.Scatter(x=days, y=visitors, name="visitors"),
+                              go.Scatter(x=days, y=registered_users, name="registered users")]
+                        )
     vis_div = plot(vis_fig, output_type='div')
-    inp_fig = go.Figure(data=go.Scatter(x=days,y=inputs, name="inputs"))
+    inp_fig = go.Figure(data=go.Scatter(x=days, y=inputs, name="inputs"))
     inp_div = plot(inp_fig, output_type='div')
-    out_fig = go.Figure(data=go.Scatter(x=days,y=outputs, name="outputs"))
+    out_fig = go.Figure(data=go.Scatter(x=days, y=outputs, name="outputs"))
     out_div = plot(out_fig, output_type='div')
-    content_dict ={"vis_div":vis_div,"inp_div":inp_div,"out_div":out_div}
-    return render(request, 'data.html', content_dict) 
+    content_dict = {"vis_div": vis_div, "inp_div": inp_div, "out_div": out_div}
+    return render(request, 'data.html', content_dict)
