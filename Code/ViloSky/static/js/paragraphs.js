76cba2afb2e2d810a3d49a216f232870ec945f09
“Orla <2391647s@student.gla.ac.uk>
2021-03-10 10:42:10 +0000

    style changes

---
+++ Code/ViloSky/static/js/paragraphs.js
@@ -0,0 +1,23 @@
+function editText(){
+    document.getElementById('editText').style.display = "block";
+    document.getElementById('view').style.display = "none";
+}
+
+function editLinks(){
+    document.getElementById('editLinks').style.display = "block";
+    document.getElementById('view').style.display = "none";
+}
+
+function editKeys(){
+    document.getElementById('editKeys').style.display = "block";
+    document.getElementById('view').style.display = "none";
+}
+
+function editActions(){
+    document.getElementById('editActions').style.display = "block";
+    document.getElementById('view').style.display = "none";
+}
+
+function cancel() {
+window.location.reload();
+}
