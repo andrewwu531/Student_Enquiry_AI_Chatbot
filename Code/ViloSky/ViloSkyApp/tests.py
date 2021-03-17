b0d2d47bb1d2dcb3ddd315d97db9280954086a31
2390362s <2390362s@student.gla.ac.uk>
2021-03-17 13:02:39 +0000

    Push missing migration and a single test
     * Create very basic test
     * Push a migration preventing from tests running
    
     TODOs:
     * Always make migrations on database changes!
    
    See #66

--- Code/ViloSky/ViloSkyApp/tests.py
+++ Code/ViloSky/ViloSkyApp/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from ViloSkyApp import models
 
+TEST_USER_PROFILE_PASSWORD = "MySecurePassword"
 # Create your tests here.
+
+
+def create_test_user_profile():
+    u = models.CustomUser.objects.get_or_create(email="a@a.com")[0]
+    u.set_password(TEST_USER_PROFILE_PASSWORD)
+    u.save()
+    user = models.UserProfile.objects.get_or_create(user=u)[0]
+    user.save()
+    return user
+
+
+class ActionPlanTests(TestCase):
+    def test_no_listed_plans_on_plain_user(self):
+        client = Client()
+        user_profile = create_test_user_profile()
+        client.login(username=user_profile.user.email,
+                     password=TEST_USER_PROFILE_PASSWORD)
+        response = client.get(reverse('actions'))
+        self.assertContains(response, "No action plans found")
