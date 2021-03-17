1748a29b7c5ffbe7b2b4cd377688d62e864b2719
Selim Hadjadj-Aoul <2396794h@student.gla.ac.uk>
2021-03-17 14:48:38 +0000

    Update settings.py

--- Code/ViloSky/ViloSky/settings.py
+++ Code/ViloSky/ViloSky/settings.py
@@ -79,11 +79,15 @@ WSGI_APPLICATION = 'ViloSky.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'CONN_MAX_AGE': 500,
-    }
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'ci',
+       'USER': 'postgres',
+       'PASSWORD': 'postgres',
+       'HOST': 'postgres',
+       'PORT': '5432',
+   },
 }
+
 
 
 # Password validation
