37ed932f50385d32a9b3361ef491edab62d02eac
Selim Hadjadj-Aoul <2396794h@student.gla.ac.uk>
2021-03-17 15:07:08 +0000

    Update settings.py

--- Code/ViloSky/ViloSky/settings.py
+++ Code/ViloSky/ViloSky/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -84,7 +85,8 @@ DATABASES = {
     }
 }
 
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
